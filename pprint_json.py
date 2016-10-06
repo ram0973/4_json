2733b66cc851199f7ded25a0bd25d413911fede5
Ramil Yabbarov <ram0973@gmail.com>
2016-10-06 11:44:36 +0300

    continue

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,22 @@
+# -*- coding: utf-8 -*-
 import json
+from sys import platform
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, mode='r', encoding="utf-8") as file:
+        return json.load(file)
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+
+    if platform == 'win32':
+        import win_unicode_console
+        win_unicode_console.enable()
+    data = load_data('Бары.json')
+    pretty_print_json(data)
+
