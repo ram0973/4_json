70470515dad05254954f789888fc692583ad5418
Ramil Yabbarov <ram0973@gmail.com>
2016-10-06 12:01:53 +0300

    JSON_FILE const added

--- pprint_json.py
+++ pprint_json.py
@@ -2,6 +2,7 @@
 import json
 from sys import platform
 
+JSON_FILE = 'Бары.json'
 
 def load_data(filepath):
     with open(filepath, mode='r', encoding="utf-8") as file:
@@ -17,5 +18,5 @@ if __name__ == '__main__':
     if platform == 'win32':
         import win_unicode_console
         win_unicode_console.enable()
-    data = load_data('Бары.json')
+    data = load_data(JSON_FILE)
     pretty_print_json(data)
