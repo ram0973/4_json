0ba34258b3e6fc491198481b79e92310272d42bb
ram0973 <ram0973@gmail.com>
2016-10-16 15:48:31 +0300

    fixes

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
-# 4_json
+# Решение задачи [№4](https://devman.org/challenges/4/) с сайта 
+[devman.org](https://devman.org)
 
-Cкрипт, который на вход принимает путь до файла, в котором хранится json и выводит
-его содержимое в консоль в удобном формате (это называется pretty print).
+## Условие задачи:
+
+Написать скрипт, который на вход принимает путь до файла, в котором 
+хранится json и выводит его содержимое в консоль в удобном формате 
+(это называется pretty print).
+
+## Системные требования
+
+```
+Python 3.5.2+
+Внешний модуль requests
+Внешний модуль win-unicode-console
+Внешний модуль colorama
+```
+
+## Установка
+
+Windows
+
+```    
+git clone https://github.com/ram0973/4_json.git
+cd 4_json
+pip install -r requirements.txt
+```
+
+Linux
+```    
+git clone https://github.com/ram0973/4_json.git
+cd 4_json
+pip3 install -r requirements.txt
+```
+    
+## Описание работы
+Пользователь вводит путь к json файлу как аргумент.
+Пример: pprint_json.py --json Бары.json
+Скрипт печатает данные из файла с красивым форматированием. 
+    
+## Запуск
+
+Windows
+
+python pprint_json.py --json ИмяФайла
+ 
+Linux
+ 
+python3 pprint_json.py --json ИмяФайла
+
+## Лицензия
+
+[MIT](http://opensource.org/licenses/MIT)
--- pprint_json.py
+++ pprint_json.py
@@ -1,36 +1,39 @@
 # -*- coding: utf-8 -*-
 import argparse
 import json
-import os
 import sys
+from colorama import Style, Fore
 
 
 def load_win_unicode_console():
+    """
+    Включаем правильное отображение unicode в консоли под MS Windows
+    и раскрашивание символов
+    """
     if sys.platform == 'win32':
         import win_unicode_console
         win_unicode_console.enable()
+        from colorama import init
+        init()  # colorama
 
 
-def get_named_argument(arg_name: str) -> str:
-    if len(sys.argv) > 1:
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--' + arg_name)
-        return getattr(parser.parse_args(sys.argv[1:]), arg_name)
+def get_json_argument():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--json', help='Введите путь к файлу .json')
+    if parser.parse_args().json:
+        return parser.parse_args().json
     else:
-        print('Введите параметр в формате --%s Значение' % arg_name)
+        parser.print_help()
         exit(1)
 
 
-def load_data(filepath: str):
-    if os.path.isfile(filepath):
-        try:
-            with open(filepath, mode='r', encoding="utf-8") as file:
+def load_json_data(file_path: str):
+    try:
+        with open(file_path, mode='r', encoding="utf-8") as file:
                 return json.load(file)
-        except PermissionError:
-            print('У вас нет прав доступа к файлу')
-            exit(1)
-    else:
-        print('Файл не найден')
+    except OSError as error:
+        print(Fore.RED+Style.BRIGHT, 'Ошибка: ', error.strerror, ' в файле: ',
+              error.filename)
         exit(1)
 
 
@@ -41,6 +44,6 @@ def pretty_print_json(data):
 if __name__ == '__main__':
 
     load_win_unicode_console()
-    file_path = get_named_argument('json')
-    data = load_data(file_path)
-    pretty_print_json(data)
+    json_file_path = get_json_argument()
+    json_data = load_json_data(json_file_path)
+    pretty_print_json(json_data)
